2cb6093e726458bb4f7b0948ccd9a9f8509a5ea9
Shivank Kapur <shivankkapur2004@gmail.com>
2024-10-03 15:59:00 +0530

    fixed connect wallet issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import '@rainbow-me/rainbowkit/styles.css';
 import './index.css'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+import App from './App';
+import { config } from './wagmi';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
 )
--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,8 +1,28 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { arbitrum, oasisTestnet, sapphireTestnet } from 'wagmi/chains';
+import { getDefaultConfig, Chain } from '@rainbow-me/rainbowkit';
+import { arbitrumSepolia, sapphireTestnet } from 'wagmi/chains';
+
+// const OasisSapphireTestnet = {
+//     id: 43_114,
+//     name: 'Avalanche',
+//     iconUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5805.png',
+//     iconBackground: '#fff',
+//     nativeCurrency: { name: 'Avalanche', symbol: 'AVAX', decimals: 18 },
+//     rpcUrls: {
+//       default: { http: ['https://api.avax.network/ext/bc/C/rpc'] },
+//     },
+//     blockExplorers: {
+//       default: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+//     },
+//     contracts: {
+//       multicall3: {
+//         address: '0xca11bde05977b3631167028862be2a173976ca11',
+//         blockCreated: 11_907_934,
+//       },
+//     },
+//   } as const satisfies Chain;
 
 export const config = getDefaultConfig({
   appName: 'oasis-router',
   projectId: 'a0ed69f908aeabb71c9e9ae5fde2645e',
-  chains: [arbitrum, oasisTestnet, sapphireTestnet],
+  chains: [arbitrumSepolia, sapphireTestnet],
 });
